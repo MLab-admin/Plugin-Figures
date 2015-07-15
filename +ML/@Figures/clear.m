d043b3a47f9ed2cd04afba23d5f6771310f01e70
Raphaël Candelier <raphael.candelier@upmc.fr>
2015-07-15 18:08:46 +0200

    Update Help of ML.Figures.clear

--- +ML/@Figures/clear.m
+++ +ML/@Figures/clear.m
@@ -1,10 +1,10 @@
 function h = clear(this, varargin)
-%[Figures].clear Clear figures by name.
-%   [Figures].CLEAR(FNAME1, FNAME2, ...) Clears the figure with name
+%ML.Figures.clear Clear figure(s)
+%   ML.Figures.clear(FNAME1, FNAME2, ...) Clears the figure with name
 %   FNAME1, FNAME2, etc. If there is no figure correponding to a name, it 
 %   is created.
 %
-%   See alos: ML.Figures.
+%   See als:o ML.Figures
 
 h = this.select(varargin{:});
 for i = 1:numel(h)
