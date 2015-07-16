351a3a4c6550f24ef4c4387fef335ca65dd519c3
MLab <raphael@candelier.fr>
2015-07-16 09:44:33 +0200

    Update ML.isfigure help

--- +ML/isfigure.m
+++ +ML/isfigure.m
@@ -1,5 +1,5 @@
 function out = isfigure(fig)
-%ML.Figures.isfigure 
+%ML.Figures.isfigure Check if the input is a figure
 
 out = (verLessThan('matlab', '8.4.0.150421') && isnumeric(fig)) || isa(fig, 'matlab.ui.Figure');
     
